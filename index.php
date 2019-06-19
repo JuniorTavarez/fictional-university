77ef947434f201feb38a95938941216388a6b2b2
Junior <jtavarez8@gmail.com>
2019-06-19 05:54:22 -0400

    learning array in php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-this is our completely amazing theme
+<!-- <?php 
+function myFirstfunction($name, $color){
+    echo "<p>Hi, my name is $name and my favorite color is $color</p>";
+}
+
+
+
+myFirstfunction('Junior', 'Blue')
+?>
+
+<h1> <?php bloginfo('name');?> </h1>
+<p> <?php bloginfo('description');?> </p> -->
+
+<?php
+$names = array('Brad', 'Junior', 'Juan', 'Mark')
+
+
+?>
+<?php
+for ($i = 0; $i < (count($names)); $i++) {
+  echo "<h1> Hi, my name is $names[$i]";
+  } ?>
+
