b49dddd3901588feffc270a1f6991f7bc8a401a8
Junior <jtavarez8@gmail.com>
2019-06-26 18:43:55 -0400

    finish professor post type

--- functions.php
+++ functions.php
@@ -11,7 +11,37 @@ add_action( 'wp_enqueue_scripts', 'university_files');
 
 function university_features() {
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true );
 }
 
 add_action('after_setup_theme', 'university_features');
-?>
+
+
+
+
+function university_adjust_queries($query){
+    if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()){
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('post_per_page', -1);
+    };
+
+    if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()){
+        $today = date('Ymd');
+        $query->set( 'meta_key', 'event_date');
+        $query->set( 'orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array (
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+        ));
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
--- page.php
+++ page.php
@@ -8,7 +8,7 @@ while(have_posts()){
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title()?></h1>
       <div class="page-banner__intro">
-        <p>Don't forget to replace me later</p>
+        <p>Dont forget to replace me later</p>
       </div>
     </div>  
   </div>
